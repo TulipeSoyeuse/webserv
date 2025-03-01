9d3ff928ba5f038e9660bff9cbc517e6dab0d6ef
Tulipe <romain.dupeux24@gmail.com>
2025-03-01 15:50:19 +0100

    barebone setup for unittesting

--- put.py
+++ put.py
@@ -7,14 +7,17 @@ BASE_URL = "http://localhost:9997"
 
 class TestRequestMethods(unittest.TestCase):
     def test_get(self):
-        response = requests.get(BASE_URL, headers=("Host", "www.webserv_test.fr"))
-        self.assertEqual(response.status_code == 200)
-        # self.assertEqual()
+        # ------------------ BASIC REQUEST : index ----------------
+        response = requests.get(BASE_URL, headers={"Host": "www.webserv_test.fr"})
+        self.assertEqual(response.status_code, 200)
+        with open("site-test3/index.html") as f:
+            self.assertEqual(f.read(), response.content.decode())
+        # ------------------ BASIC REQUEST : string arg ----------------
 
 
 # with open("kitten.jpg", "rb") as f:
 #     requests.put("http://localhost:9997/upload/python_script/kitten.jpg", data=f.read())
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
+    unittest.main()
